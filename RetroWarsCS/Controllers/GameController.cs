c84fa65da916305f63deaea955a39b4c61b39672
Vladislav Kotsev <vladygk8@gmaila.com>
2023-08-06 01:30:10 +0300

    Add toastr

--- RetroWarsCS/Controllers/GameController.cs
+++ RetroWarsCS/Controllers/GameController.cs
@@ -1,20 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿namespace RetroWars.Web.Controllers;
+
+using Microsoft.AspNetCore.Mvc;
 using RetroWars.Services.Data.Contracts;
+using static Common.NotificationMessagesConstants;
 
-namespace RetroWars.Web.Controllers
+public class GameController : Controller
 {
-    public class GameController : Controller
+    private readonly IGameService gameService;
+    public GameController(IGameService gameService)
     {
-        private readonly IGameService gameService;
-        public GameController(IGameService gameService)
-        {
-            this.gameService = gameService;
-        }
-        public async Task<IActionResult > All()
-        {
-            var allGames = await this.gameService.GetAllGamesAsync();
-
-            return View(allGames);
-        }
+        this.gameService = gameService;
+    }
+    public async Task<IActionResult> All()
+    {
+        var allGames = await this.gameService.GetAllGamesAsync();
+        TempData[InformationMessage] = "Yaba daba du";
+        return View(allGames);
     }
 }
+
